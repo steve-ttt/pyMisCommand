82359d0ef4650c73e2295abf34ceda8f6415edac
steve-ttt <stephen.thorley@gmail.com>
2016-10-26 15:52:54 +1100

    Add files via upload

--- misCommand.py
+++ misCommand.py
@@ -5,10 +5,7 @@ from pygame.locals import *
 # Globals
 WIDTH  = 800
 HEIGHT = 600
-
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Hello World!')
+EXPLSPEED = 2               #explosion speed
 
 # Colour Constants
 BLACK = (  0,   0,   0)
@@ -17,10 +14,15 @@ RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 LBLUE = ( 98,   7, 250)
+YELLOW =(255, 255,   0)
 
-FPS = 30 # frames per second setting
+FPS = 30                    # frames per second setting
 fpsClock = pygame.time.Clock()
 
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Missile Command!')
 
 # draw on the surface object
 DISPLAYSURF.fill(BLACK)
@@ -40,39 +42,36 @@ textRectObj.center = (50, 10)
 
 # images
 catImg = pygame.image.load('cat.png')
+bground = pygame.image.load('assets/bground.png')
 catx = 10
 caty = 10
 direction = 'right'
 
-explosions=[]
+explosions=[]  #[mousex, mousey, pygame.time.get_ticks(), 5, False] ie: x, y, time start, start size, max size reached true/false
 
 # run the game loop
 while True:
     DISPLAYSURF.fill(BLACK)
+    DISPLAYSURF.blit(bground, (0,0))
     DISPLAYSURF.blit(textSurfaceObj, textRectObj)
-    DISPLAYSURF.blit(catImg, (catx, caty))
     
-    
-    if direction == 'right':
-        catx += 5
-        if catx == 280:
-            direction = 'down'
-    elif direction == 'down':
-        caty += 5
-        if caty == 220:
-            direction = 'left'
-    elif direction == 'left':
-        catx -= 5
-        if catx == 10:
-            direction = 'up'
-    elif direction == 'up':
-         caty -= 5
-         if caty == 10:
-             direction = 'right'
     
     # draw explosions from list
     for expl in explosions:
-        pygame.draw.ellipse(DISPLAYSURF, RED, (expl[0], expl[1], 10, 10), 1)
+        pygame.draw.ellipse(DISPLAYSURF, RED, (expl[0], expl[1], expl[3], expl[3]), 1)
+        if expl[3] <= 80 and expl[4] == False:
+            expl[3] = expl[3] + EXPLSPEED
+        elif expl[3] == 5 and expl[4] == True:
+            explosions.remove(expl)
+        else:
+            expl[4] = True
+            expl[3] = expl[3] - EXPLSPEED
+    
+    #Delete explosions from list if older than 3 sec
+    #for expl in explosions:
+    #   if expl[2] <= pygame.time.get_ticks() - 3000:
+    #        explosions.remove(expl)
+    
     
     # event handler
     for event in pygame.event.get():
@@ -85,9 +84,10 @@ while True:
             textSurfaceObj = fontObj.render(mouseStr, True, GREEN, BLACK)
         elif event.type == MOUSEBUTTONUP:
             mousex, mousey = event.pos
-            pygame.draw.line(DISPLAYSURF, LBLUE, (400,600), (mousex, mousey))
-            explosions.append([mousex, mousey])
-            pygame.draw.ellipse(DISPLAYSURF, RED, (mousex - 20, mousey -40, 40, 80), 1)
+            pygame.draw.line(DISPLAYSURF, YELLOW, (400,600), (mousex, mousey), 2)
+            explosions.append([mousex, mousey, pygame.time.get_ticks(), 5, False])    #set X, y, and time of creation
+            pygame.draw.ellipse(DISPLAYSURF, RED, (mousex - 40, mousey -20, 80, 40), 1)
+            
     
     pygame.display.update()
 
